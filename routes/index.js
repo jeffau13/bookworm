3128b83f20f5a2f249359c9d667301ce4b9fe0ee
jeffau13 <jeffau13@gmail.com>
2019-03-02 19:10:31 -0500

    init commit. Initial routes are set up

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+// GET /
+router.get('/', function(req, res, next) {
+  return res.render('index', { title: 'Home' });
+});
+
+// GET /about
+router.get('/about', function(req, res, next) {
+  return res.render('about', { title: 'About' });
+});
+
+// GET /contact
+router.get('/contact', function(req, res, next) {
+  return res.render('contact', { title: 'Contact' });
+});
+
+// GET /register
+router.get('/register', (req,res,next)=>{
+  res.send('Register today!');
+  // res.render('register', {title: 'Register'});
+});
+module.exports = router;
